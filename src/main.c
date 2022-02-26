ca5f8801fc4995c81faa94714db1137dff1cb43d
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2022-02-27 00:43:15 +0300

    Project logic

---
+++ src/main.c
@@ -0,0 +1,17 @@
+/**
+* Project Logic:
+* Start the game
+* Load Game window
+* If window fails to load: End the game
+* Else:
+* Initialize game properties like:
+* Where does the player start
+* His speed of movement
+* Everything is initalised to 0 for the first attempt. 
+* The player has three chances to move without touching the wall.
+* If he does, he loses round 1.
+* On round two, he resumes from his previous position
+* The game has three rounds.
+* After the third attempt, Game over is displayed and the game quits.
+*
+*/
