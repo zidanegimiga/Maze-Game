f666cdbc1b1375082a2b85c95a4920c2bf3d1192
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2022-02-27 08:15:03 +0300

    initializing the widow creation function

--- headers/header.h
+++ headers/header.h
@@ -3,5 +3,14 @@
 
 #include <stdio.h>
 #include <SDL2/SDL.H>
+#include <stdbool.h>
+
+/* constants */
+#define TILE_SIZE 64
+#define SCREEN_WIDTH (MAP_NUM_COLS * TILE_SIZE)
+#define SCREEN_HEIGHT (MAP_NUM_ROWS * TILE_SIZE)
+
+
+bool initializeWindow(void);
 
 #endif /* HEADER_H */
