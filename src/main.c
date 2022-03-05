bec4b5f585bcc0b160dbad420e68bfd14528a787
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2022-03-05 19:42:58 +0300

    game setup

--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
+#include "../headers/header.h"
+
 /**
 * Project Logic:
 * Start the game
@@ -15,3 +17,22 @@
 * After the third attempt, Game over is displayed and the game quits.
 *
 */
+
+/**
+ * setup_game - initialize player variables and load wall textures
+ *
+*/
+
+void setup_game(void)
+{
+
+	player.x = SCREEN_WIDTH / 2;
+	player.y = SCREEN_HEIGHT / 2;
+	player.width = 1;
+	player.height = 30;
+	player.walkDirection = 0;
+	player.walkSpeed = 100;
+	player.turnDirection = 0;
+	player.turnSpeed = 45 * (PI / 180);
+	player.rotationAngle = PI / 2;
+}
